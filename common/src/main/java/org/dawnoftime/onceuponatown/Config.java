dd0a1e762cf9b47d5dc81ccf602ecd359fd1e624
PierreChag <p.chagnon@orange.fr>
2024-12-02 00:23:14 +0100

    Maybe the last error ?

--- common/src/main/java/org/dawnoftime/onceuponatown/Config.java
+++ common/src/main/java/org/dawnoftime/onceuponatown/Config.java
@@ -24,36 +24,19 @@ public class Config {
     /** Maximum vertical difference accepted between the door Y value, and any block Y value, on the border of the Building.*/
     public static final int MAXI_Y_DIFFERENCE = 10;
     /** Maximum size of the side of a squared garden. If a Bud available space in one of the 2 directions is smaller, it creates a MapGarden.*/
-    public static final int SIDE_SIZE_MAX_GARDEN = 10;
+    public static final int BUD_MINIMAL_SPACE = 10;
     /** Minimum spacing between paths when the central building is placed.*/
     public static final int MINI_PATH_SPACE = 20;
     /** Probability of placing a path when adding a bud.*/
-    public static final float PATH_SPAWN_RATE = 0.25F;
+    public static final float ROAD_SPAWN_RATE = 0.25F;
     /** Probability that a new path is a big path.*/
-    public static final float BIG_PATH_SPAWN_RATE = 0.3F;
+    public static final float WIDE_ROAD_SPAWN_RATE = 0.3F;
     /** Probability that a path stops growing when a building is placed next to it.*/
     public static final float PATH_STOP_RATE = 0.15F;
     /** Tick rate of the towns per second.*/
     public static final int TOWN_TICK_RATE_SECONDS = 5;
-    // Width of small paths
-    public static final int SMALL_WIDTH = 2;
-    // Width of large paths
-    public static final int BIG_WIDTH = 4;
-    // Default size of paths, as well as the minimum size below which an area is transformed into a garden
-    public static final int DEFAULT_PATH_LENGTH = 10;
-    // Maximum vertical difference accepted between the door Y value, and any block Y value, on the border of the Building.
-    public static final int MAXI_Y_DIFFERENCE = 10;
-    // Maximum size of the side of a squared garden. If a Bud available space in one of the 2 directions is smaller, it creates a MapGarden.
-    public static final int BUD_MINIMAL_SPACE = 10;
-    // Minimum spacing between paths when the central building is placed
-    public static final int MINI_PATH_SPACE = 20;
-    // Probability of placing a path when adding a bud
-    public static final float ROAD_SPAWN_RATE = 0.25F;
-    // Probability that a new path is a big path
-    public static final float WIDE_ROAD_SPAWN_RATE = 0.3F;
-    // Probability that a path stops growing when a building is placed next to it
-    public static final float PATH_STOP_RATE = 0.15F;
-    public static int MAX_TOWN_INVENTORY_STACK_SIZE = 1000;
+    /** Maximal amount of an item a town can have. */
+    public static int MAX_TOWN_INVENTORY_STACK_SIZE = 10000;
 
     public record FakeConfig<T>(T configValue) {
         public T get() {
