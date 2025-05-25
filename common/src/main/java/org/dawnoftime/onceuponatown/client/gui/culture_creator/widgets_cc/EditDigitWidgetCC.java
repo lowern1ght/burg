35d60f0e427a9b6a7af1a85ef43beaf3f47dd168
pchag <p.chagnon@orange.fr>
2025-05-25 12:52:53 +0200

    Level menu finalized !

--- common/src/main/java/org/dawnoftime/onceuponatown/client/gui/culture_creator/widgets_cc/EditDigitWidgetCC.java
+++ common/src/main/java/org/dawnoftime/onceuponatown/client/gui/culture_creator/widgets_cc/EditDigitWidgetCC.java
@@ -22,7 +22,7 @@ public class EditDigitWidgetCC extends WidgetCC {
      */
     public EditDigitWidgetCC(int posX, Component hint, Font font, boolean isInt) {
         this.textButton = new LeftAlignTextButton(posX + WIDGET_ZONE_X, 0, WIDGET_ZONE_WIDTH - 3 * WIDGET_HEIGHT - 1, WIDGET_HEIGHT, hint);
-        this.editBox = new EditBox(font, posX + WIDGET_ZONE_X + WIDGET_ZONE_WIDTH - 3 * WIDGET_HEIGHT + 1, 0, 3 * WIDGET_HEIGHT - 2, WIDGET_HEIGHT - 2, Component.literal("...")) {
+        this.editBox = new EditBox(font, posX + WIDGET_ZONE_X + WIDGET_ZONE_WIDTH - 3 * WIDGET_HEIGHT + 1, 0, 3 * WIDGET_HEIGHT - 2, WIDGET_HEIGHT - 2, EMPTY_EDIT_BOX) {
             // We must edit the setY function because for some reason, MC devs decided that the actual border of this widget should be bigger than its size...
             @Override
             public void setY(int y) {
@@ -39,6 +39,7 @@ public class EditDigitWidgetCC extends WidgetCC {
                 return super.charTyped(codePoint, modifiers);
             }
         };
+        this.editBox.setHint(EMPTY_EDIT_BOX);
     }
 
     @Override
--- common/src/main/java/org/dawnoftime/onceuponatown/client/gui/culture_creator/widgets_cc/WidgetCC.java
+++ common/src/main/java/org/dawnoftime/onceuponatown/client/gui/culture_creator/widgets_cc/WidgetCC.java
@@ -1,10 +1,12 @@
 package org.dawnoftime.onceuponatown.client.gui.culture_creator.widgets_cc;
 
 import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public abstract class WidgetCC {
+    protected static final Component EMPTY_EDIT_BOX = Component.literal("...");
 
     public WidgetCC() {}
 
