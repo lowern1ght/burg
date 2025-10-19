82711b0d33acaaba6f762a5553063ef818277fdc
pchag <p.chagnon@orange.fr>
2025-10-19 15:34:40 +0200

    Culture creator point is now working properly.

--- common/src/main/java/org/dawnoftime/onceuponatown/client/renderer/CultureCreatorRenderer.java
+++ common/src/main/java/org/dawnoftime/onceuponatown/client/renderer/CultureCreatorRenderer.java
@@ -9,6 +9,7 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.Tag;
 import net.minecraft.resources.ResourceLocation;
@@ -17,20 +18,24 @@ import org.dawnoftime.onceuponatown.entity.CultureCreatorEntity;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
 
+import static org.dawnoftime.onceuponatown.Ouat.MOD_ID;
+
 public class CultureCreatorRenderer extends EntityRenderer<CultureCreatorEntity> {
+    private static final ResourceLocation WHITE_TEXTURE = new ResourceLocation(MOD_ID, "textures/entity/culture_creator_entity.png");
+
     public CultureCreatorRenderer(EntityRendererProvider.Context context) {
         super(context);
     }
 
     @Override
     public @NotNull ResourceLocation getTextureLocation(@NotNull CultureCreatorEntity cultureCreatorEntity) {
-        return null;
+        return WHITE_TEXTURE;
     }
 
     @Override
     public void render(@NotNull CultureCreatorEntity entity, float entityYaw, float partialTick, @NotNull PoseStack poseStack, @NotNull MultiBufferSource buffer, int packedLight) {
         poseStack.translate(0, 0.5F, 0);
-        VertexConsumer consumer = buffer.getBuffer(RenderType.debugFilledBox());
+        VertexConsumer consumer = buffer.getBuffer(RenderType.entityTranslucentCull(WHITE_TEXTURE));
         Matrix4f matrix = poseStack.last().pose();
         if (isPairedWithClientPlayerItem(entity)) {
             renderTranslucentBox(consumer, matrix, 1.1F, 1.0F, 1.0F, 1.0F, 0.6F);
@@ -39,23 +44,40 @@ public class CultureCreatorRenderer extends EntityRenderer<CultureCreatorEntity>
         }
     }
 
-    public static void renderTranslucentBox(VertexConsumer consumer, Matrix4f matrix, float size, float r, float g, float b, float alpha) {
-        float max = size / 2.0F;
-        float min = -max;
-        consumer.vertex(matrix, min, min, min).color(r, g, b, alpha).endVertex();
-        consumer.vertex(matrix, max, min, min).color(r, g, b, alpha).endVertex();
-        consumer.vertex(matrix, min, min, max).color(r, g, b, alpha).endVertex();
-        consumer.vertex(matrix, max, min, max).color(r, g, b, alpha).endVertex();
-        consumer.vertex(matrix, max, max, max).color(r, g, b, alpha).endVertex();
-        consumer.vertex(matrix, max, min, min).color(r, g, b, alpha).endVertex();
-        consumer.vertex(matrix, max, max, min).color(r, g, b, alpha).endVertex();
-        consumer.vertex(matrix, min, min, min).color(r, g, b, alpha).endVertex();
-        consumer.vertex(matrix, min, max, min).color(r, g, b, alpha).endVertex();
-        consumer.vertex(matrix, min, min, max).color(r, g, b, alpha).endVertex();
-        consumer.vertex(matrix, min, max, max).color(r, g, b, alpha).endVertex();
-        consumer.vertex(matrix, max, max, max).color(r, g, b, alpha).endVertex();
-        consumer.vertex(matrix, min, max, min).color(r, g, b, alpha).endVertex();
-        consumer.vertex(matrix, max, max, min).color(r, g, b, alpha).endVertex();
+    private void renderTranslucentBox(VertexConsumer consumer, Matrix4f matrix, float size, float r, float g, float b, float alpha) {
+        float half = size / 2f;
+        // Front, Back, Left, Right
+        addVerticalFace(consumer, matrix, -half, -half, -half, half, half, -half, r, g, b, alpha, 0, 1);
+        addVerticalFace(consumer, matrix, half, -half, half, -half, half, half, r, g, b, alpha, 0, -1);
+        addVerticalFace(consumer, matrix, -half, -half, half, -half, half, -half, r, g, b, alpha, -1, 0);
+        addVerticalFace(consumer, matrix, half, -half, -half, half, half, half, r, g, b, alpha, 1, 0);
+        // Top
+        makeVertex(consumer, matrix, -half, half, -half, r, g, b, alpha, 0, 0, 0, 1, 0);
+        makeVertex(consumer, matrix, -half, half, half, r, g, b, alpha, 0, 1, 0, 1, 0);
+        makeVertex(consumer, matrix, half, half, half, r, g, b, alpha, 1, 1, 0, 1, 0);
+        makeVertex(consumer, matrix, half, half, -half, r, g, b, alpha, 1, 0, 0, 1, 0);
+        // Bottom
+        makeVertex(consumer, matrix, -half, -half, -half, r, g, b, alpha, 0, 0, 0, 1, 0);
+        makeVertex(consumer, matrix, half, -half, -half, r, g, b, alpha, 0, 1, 0, 1, 0);
+        makeVertex(consumer, matrix, half, -half, half, r, g, b, alpha, 1, 1, 0, 1, 0);
+        makeVertex(consumer, matrix, -half, -half, half, r, g, b, alpha, 1, 0, 0, 1, 0);
+    }
+
+    private void addVerticalFace(VertexConsumer consumer, Matrix4f matrix, float x1, float y1, float z1, float x2, float y2, float z2, float r, float g, float b, float alpha, int nx, int nz) {
+        makeVertex(consumer, matrix, x1, y1, z1, r, g, b, alpha, 0, 0, nx, 0, nz);
+        makeVertex(consumer, matrix, x1, y2, z1, r, g, b, alpha, 0, 1, nx, 0, nz);
+        makeVertex(consumer, matrix, x2, y2, z2, r, g, b, alpha, 1, 1, nx, 0, nz);
+        makeVertex(consumer, matrix, x2, y1, z2, r, g, b, alpha, 1, 0, nx, 0, nz);
+    }
+
+    private void makeVertex(VertexConsumer consumer, Matrix4f matrix, float x, float y, float z, float r, float g, float b, float alpha, float u, float v, int nx, int ny, int nz) {
+        consumer.vertex(matrix, x, y, z)
+                .color(r, g, b, alpha)
+                .uv(u, v)
+                .overlayCoords(OverlayTexture.NO_OVERLAY)
+                .uv2(0x00F000F0)
+                .normal(nx, ny, nz)
+                .endVertex();
     }
 
     private static boolean isPairedWithClientPlayerItem(@NotNull CultureCreatorEntity entity) {
