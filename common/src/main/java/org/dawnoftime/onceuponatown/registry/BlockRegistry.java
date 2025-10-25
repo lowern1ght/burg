3b3b5d440e8f5c4cbce099bb1873f995e85df294
pchag <p.chagnon@orange.fr>
2025-10-25 15:03:23 +0200

    feat : Transparent particles for block and sound.

--- common/src/main/java/org/dawnoftime/onceuponatown/registry/BlockRegistry.java
+++ common/src/main/java/org/dawnoftime/onceuponatown/registry/BlockRegistry.java
@@ -4,6 +4,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import org.dawnoftime.onceuponatown.block.CultureCreatorBlock;
 import org.dawnoftime.onceuponatown.block.SlabPathBlock;
@@ -16,7 +17,7 @@ public abstract class BlockRegistry {
     public static BlockRegistry REGISTRY;
 
     public final Supplier<Block> DIRT_PATH_SLAB = register("dirt_path_slab", () -> new SlabPathBlock(Block.Properties.copy(Blocks.DIRT_PATH)));
-    public final Supplier<CultureCreatorBlock> CULTURE_CREATOR_BLOCK = registerWithItem("culture_creator", () -> new CultureCreatorBlock(BlockBehaviour.Properties.of().instabreak().noCollission()), (block -> new CultureCreatorItem(block, new Item.Properties())));
+    public final Supplier<CultureCreatorBlock> CULTURE_CREATOR_BLOCK = registerWithItem("culture_creator", () -> new CultureCreatorBlock(BlockBehaviour.Properties.of().instabreak().noCollission().sound(SoundType.AMETHYST)), (block -> new CultureCreatorItem(block, new Item.Properties())));
 
     public <T extends Block> Supplier<T> register(String id, Supplier<T> block) {
         return this.registerWithItem(id, block, (T blockObject) -> new BlockItem(blockObject, new Item.Properties()));
