1307723f0bd8e2343fff37a086436e995d22d9af
TheGoldenWorld <dawnoftime.inc@gmail.com>
2026-06-05 23:23:46 +0200

    oups

--- onceuponatown_old_state/common/src/main/java/org/dawnoftime/onceuponatown/client/gui/tooltip/TradeItemTooltip.java
+++
@@ -1,8 +0,0 @@
-package org.dawnoftime.onceuponatown.client.gui.tooltip;
-
-import net.minecraft.world.inventory.tooltip.TooltipComponent;
-import net.minecraft.world.item.ItemStack;
-
-public record TradeItemTooltip(ItemStack a, ItemStack b, ItemStack c) implements TooltipComponent {
-
-}
